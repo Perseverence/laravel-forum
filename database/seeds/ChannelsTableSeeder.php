02593453e34e3d54b0cb3a68a772e79188fc5145
perseverence <valentin_dobrica@yahoo.com>
2019-12-05 22:26:56 +0200

    test

---
+++ database/seeds/ChannelsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Channel;
+use Illuminate\Support\Str;
+
+class ChannelsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Channel::create([
+            'name' => 'Laravel 5.8',
+            'slug' => Str::slug('Laravel 5.8')
+        ]);
+
+        Channel::create([
+            'name' => 'Vue js 3',
+            'slug' => Str::slug('Vue js 3')
+        ]);
+
+        Channel::create([
+            'name' => 'Angular 7',
+            'slug' => Str::slug('Angular 7')
+        ]);
+
+        Channel::create([
+            'name' => 'Node js',
+            'slug' => Str::slug('Node js')
+        ]);
+    }
+}
